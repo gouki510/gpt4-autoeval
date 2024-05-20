a9b190884272fa57996d719ad12c34e0edda096a
gouki.minegishi <gouki.minegishi@goukiminegishinoMacBook-Pro.local>
2024-05-21 01:32:08 +0900

    modify path

--- src/lib/client_openai.py
+++ src/lib/client_openai.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 with open("./assets/prompt_eval.txt") as f:
     template_prompt = f.read()
 
-with open("/run/secrets/OPENAI_API_KEY") as f:
+with open("./secrets/OPENAI_API_KEY") as f:
     OPENAI_API_KEY = f.read()
 
 client = OpenAI(api_key=OPENAI_API_KEY)
